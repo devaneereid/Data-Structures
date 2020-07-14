f557730fddebba5969055f48e59dd72407214535
Devanee <devaneereid@gmail.com>
2020-07-14 14:21:19 -0600

    finshed stack.py, tests now passing

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -28,6 +28,16 @@ class DoublyLinkedList:
     """
     def add_to_head(self, value):
         pass
+        # create instance of ListNode with value
+        # increment the DLL length attribute
+
+        # if DLL is empty
+            # set head and tail to new node instance
+
+        # if DLL is not empty
+            # set new node's next to current head
+            # set head's prev to new node
+            # set head to the new node
         
     """
     Removes the List's current head node, making the
@@ -36,6 +46,17 @@ class DoublyLinkedList:
     """
     def remove_from_head(self):
         pass
+        # store the value of the head
+        # decrement the length of the DLL
+        # delete the head
+            # if head.next is not None
+                # set head.next's prev to None
+                # set head to head.next
+            # else (if head.next is None)
+                # set head to None
+                # set tail to None
+
+        # return the value
             
     """
     Wraps the given value in a ListNode and inserts it 
@@ -44,6 +65,17 @@ class DoublyLinkedList:
     """
     def add_to_tail(self, value):
         pass
+
+        # create instance of ListNode with value
+        # increment the DLL length attribute
+
+        #if DLL is empty
+            # set head and tail to the new node instance
+
+        # if DLL is not empty
+            # set new node's prev to current tail
+            # set tails's next to new node
+            # set tail to the new node
             
     """
     Removes the List's current tail node, making the 
@@ -52,6 +84,17 @@ class DoublyLinkedList:
     """
     def remove_from_tail(self):
         pass
+        # store the value of the tail
+            # decrement the length of the DLL
+            # delete the tail
+                # if tail.prev is not None
+                    # set tail.prev's next to None
+                    # set tail to tail.prev
+                # else (if tail.prev is None)
+                    # set head to None
+                    # set tail to None
+
+            # return the value
             
     """
     Removes the input node from its current spot in the 
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,24 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+from stack_linked_list import LinkedList
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if not self.storage.head:
+            return None
+        else:
+            self.size -= 1
+            return self.storage.remove_tail()
+
